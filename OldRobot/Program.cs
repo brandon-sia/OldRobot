d65dcd4b1ad3b19283e8bd318eba9cc642daf228
Brandon Sia <brandonsia@hotmail.com>
2022-07-27 00:30:34 +1000

    Add project files.

---
+++ OldRobot/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace OldRobot // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Robot robot = new Robot();
+            for (int index = 0; index < robot.Commands.Length; index++)
+            {
+                string? input = Console.ReadLine();
+                switch (input)
+                {
+                    case "on":
+                        robot.Commands[index] = new OnCommand();
+                        break;
+                    case "off":
+                        robot.Commands[index] = new OffCommand();
+                        break;
+                    case "north":
+                        robot.Commands[index] = new NorthCommand();
+                        break;
+                    case "south":
+                        robot.Commands[index] = new SouthCommand();
+                        break;
+                    case "east":
+                        robot.Commands[index] = new EastCommand();
+                        break;
+                    case "west":
+                        robot.Commands[index] = new WestCommand();
+                        break;
+
+                }
+            }
+            Console.WriteLine();
+            robot.Run();
+        }
+    }
+}
---
+++ OldRobot/RobotCommand.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OldRobot
+{
+    public abstract class RobotCommand : Robot
+    {
+        public abstract void Run(Robot robot);
+    }
+
+    class OnCommand : RobotCommand
+    {
+        public override void Run(Robot robot)
+        {
+            robot.IsPowered = true;
+        }
+    }
+
+    class OffCommand : RobotCommand
+    {
+        public override void Run(Robot robot)
+        {
+            robot.IsPowered = false;
+        }
+    }
+    class NorthCommand : RobotCommand
+    {
+        public override void Run(Robot robot)
+        {
+            if (robot.IsPowered)
+            {
+                robot.Y += 1;
+            }
+        }
+    
+    }
+
+    class SouthCommand : RobotCommand
+    {
+        public override void Run(Robot robot)
+        {
+            if (robot.IsPowered)
+            {
+                robot.Y -= 1;
+            }
+        }
+
+    }
+    class EastCommand : RobotCommand
+    {
+        public override void Run(Robot robot)
+        {
+            if (robot.IsPowered)
+            {
+                robot.X += 1;
+            }
+        }
+
+    }
+    class WestCommand : RobotCommand
+    {
+        public override void Run(Robot robot)
+        {
+            if (robot.IsPowered)
+            {
+                robot.X -= 1;
+            }
+        }
+
+    }
+
+
+}
